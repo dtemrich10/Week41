d932953829fae03c17f20d7cdc96306cce9f6218
David <dtemrich@gmail.com>
2021-05-11 12:29:39 -0500

    Initial test4

--- testclass/com/MyTest/TestMyClass.java
+++ testclass/com/MyTest/TestMyClass.java
@@ -3,7 +3,8 @@ package com.MyTest;
 public class TestMyClass{
 
  public static void main(String[] args){
-   System.out.println("This is a test3");
- }
+   System.out.println("This is a test4");
+    }
+
 
 }
