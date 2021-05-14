c0e73964e4b31037324ec91ba7db5dd82a634d7a
dtemrich10 <42625919+dtemrich10@users.noreply.github.com>
2021-05-14 11:48:17 -0500

    Update Main.java

--- src/com/CS246_03/Main.java
+++ src/com/CS246_03/Main.java
@@ -3,6 +3,7 @@ package com.CS246_03;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("This is test for Angus");
+	System.out.println("This is test for now");
     }
 }
+for
